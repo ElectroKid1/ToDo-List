bb6abdf7eaaee359533640ba89bf441f0744664d
ElectroKid1 <44017121+ElectroKid1@users.noreply.github.com>
2019-06-26 21:34:18 -0700

    ToDo app v1.0 The notes are displayed in one line.
    here delete function is to be added,

--- ToDo app .py
+++ ToDo app .py
@@ -2,8 +2,9 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 conn=sqlite3.connect("todo.db")
-#conn.execute("create table tasks(task char(50),priority char(20)); ")
+#conn.execute("create table tasks(seq_no int primary key,task char(50),priority char(20)); ")
 root=Tk()
+root.geometry("600x600")
 #-----------------------------------------------------------------------------------------------
 #func to exit the parent window
 def exit():
@@ -12,8 +13,9 @@ def exit():
 
 #function to delete the selected record in database    
 def delete():
-    
-    print("deleted")
+    for i in lb1.curselection():
+        print(i)
+        print(lb1.get(i))
 
 #func to ADD NEW TASK in database 
 def add_task():
@@ -22,12 +24,13 @@ def add_task():
     
     #function to enter the note in database 
     def save():
+        sequence=int(entry_task_sequence.get())
         task=entry_task_description.get()
         priority=entry_task_priority.get()
-        if task=="" or priority=="":
-            messagebox.showinfo('ERROR','Please enter the details in both sections!')
+        if task=="" or priority=="" or sequence=="":
+            messagebox.showinfo('ERROR','Please enter the details in all sections!')
         else:
-            conn.execute("insert into tasks values('{}','{}');".format(task,priority))
+            conn.execute("insert into tasks values({},'{}','{}');".format(sequence,task,priority))
             print("values saved succesfully")
         
     #func to save and exit the add new task window    
@@ -35,13 +38,19 @@ def add_task():
         conn.commit()
         add_task_window.destroy()
     
-    l1=Label(add_task_window,text="Enter new task within 50 characters")
+    l1=Label(add_task_window,text="Enter the sequence of new task ")
     l1.pack()
+    entry_task_sequence=Entry(add_task_window,bd=3)
+    entry_task_sequence.pack()
+    
+    
+    l2=Label(add_task_window,text="Enter new task within 50 characters")
+    l2.pack()
     entry_task_description=Entry(add_task_window,bd=3)
     entry_task_description.pack()
     
-    l2=Label(add_task_window,text="Set the priority of the task")
-    l2.pack()
+    l3=Label(add_task_window,text="Set the priority of the task")
+    l3.pack()
     entry_task_priority=Entry(add_task_window,bd=3)
     entry_task_priority.pack()
     
@@ -59,16 +68,15 @@ bttn_del_task=Button(root,text="Delete",command=delete)
 bttn_del_task.pack()
 bttn_exit=Button(root,text="Exit window",command=exit)
 bttn_exit.pack()
-
+#-----------------------------------------------------------------------------------------
 #to display the NOTES 
 cursor=conn.execute("select * from tasks;")
 count=0
-lb1=Listbox(root,selectmode=MULTIPLE)
+lb1=Listbox(root,selectmode=MULTIPLE,width=70,height=25)
 for record in cursor:
-    for cell in record:
-        count=count+1
-        print(cell)
-        lb1.insert(count,"{}".format(cell))
+    print(record)
+    lb1.insert(count,"{}".format(record))
+    count=count+1
 lb1.pack()  
 
 
